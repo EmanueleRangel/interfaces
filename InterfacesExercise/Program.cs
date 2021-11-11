be896fdf9395a11c5f16d6b309cb8daa24eb9528
Emanuele Rangel <emanuele.rangel52@gmail.com>
2021-11-11 14:57:00 -0300

    Exercicio de sistema online de pagamentos finalizado

---
+++ InterfacesExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using InterfacesExercise.Entities;
+using InterfacesExercise.Services;
+
+
+Console.WriteLine("Enter contract data");
+Console.Write("Number: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Date: ");
+DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+Console.Write("Contract value: ");
+double totalValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Enter the number of installments: ");
+int months = int.Parse(Console.ReadLine());
+
+Contract contract = new Contract(number, date, totalValue);
+
+ContractService contractService = new ContractService(new PaypalService());
+contractService.ProcessContract(contract, months);
+
+Console.Write("Installments: ");
+foreach (Installment installment in contract.Installments)
+{
+    Console.WriteLine(installment);
+}
+
+
+
+
+
+
+
---
+++ InterfacesExercise/Services/IOnlinePaymentService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExercise.Services
+{
+    internal interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+       
+
+    }
+}
